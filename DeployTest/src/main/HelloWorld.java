4a2eadd8350011d3cb45a5f91cfa6dd590f034dd
Kinshuk Juneja <kinshuk.juneja02@gmail.com>
2017-09-21 20:39:07 -0700

    Adding Hello World coded in Java to test for deployment

---
+++ DeployTest/src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    private void printHelloWorld() {
+        System.out.print("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        new HelloWorld().printHelloWorld();
+    }
+}
