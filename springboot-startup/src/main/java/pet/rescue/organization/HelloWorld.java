043ec7c279a6e5524a6724d167c212d2b0b2dae2
chtran14 <chtran14@gmail.com>
2017-09-22 17:42:37 -0700

    fixing helloworld file

--- springboot-startup/src/main/java/pet/rescue/organization/HelloWorld.java
+++ springboot-startup/src/main/java/pet/rescue/organization/HelloWorld.java
@@ -1,25 +1,50 @@
 
 package pet.rescue.organization;
 
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+// @Controller
+
+// @SpringBootApplication
+// public class HelloWorld {
+
+// @RequestMapping("/")
+
+// @ResponseBody
+// String home() {
+// return "Hello World!";
+// }
+
+// public static void main(String[] args) {
+// SpringApplication.run(HelloWorld.class, args);
+// }
+// }
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
-@SpringBootApplication
+@EnableAutoConfiguration
 public class HelloWorld {
 
   @RequestMapping("/")
-
   @ResponseBody
   String home() {
     return "Hello World!";
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    String webPort = System.getenv("PORT");
+    if ((webPort == null) || webPort.isEmpty()) {
+      webPort = "8080";
+    }
+    System.setProperty("server.port", webPort);
     SpringApplication.run(HelloWorld.class, args);
   }
 }
