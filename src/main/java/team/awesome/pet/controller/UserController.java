b1cb53b17a477e4d9b60f2e84e692934ba720a18
Kinshuk Juneja <kinshuk.juneja02@gmail.com>
2017-10-16 15:04:52 -0700

    Added UserController

---
+++ src/main/java/team/awesome/pet/controller/UserController.java
@@ -0,0 +1,43 @@
+package team.awesome.pet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import team.awesome.pet.entity.User;
+import team.awesome.pet.service.UserService;
+
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<User> getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUserById(@PathVariable("id") int id) {
+        return this.userService.getUserById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateUser(@RequestBody User pet) {
+        this.userService.updateUser(user);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void insertUser(@RequestBody User user) {
+        this.userService.insertPet(user);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteUserById(@PathVariable("id") int id) {
+        this.userService.removeUserById(id);
+    }
+}
