51220139f57c05dadf6fc613c31309a12505b9b6
Kinshuk Juneja <kinshuk.juneja02@gmail.com>
2017-10-27 15:34:16 -0700

    Added Jpa for TrackDao

---
+++ src/main/java/team/awesome/pet/dao/jpa/JpaTrackDao.java
@@ -0,0 +1,38 @@
+package team.awesome.pet.dao.jpa;
+
+import team.awesome.pet.dao.TrackDao;
+import team.awesome.pet.model.Track;
+import java.math.BigInteger;
+import java.util.Collection;
+
+public class JpaTrackDao implements TrackDao {
+    @Override
+    public Collection<Track> getAllTracks() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Track getTrackById(BigInteger id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateTrack(Track track) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void insertTrackToDb(Track track) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeTrackById(BigInteger id) {
+        // TODO Auto-generated method stub
+
+    }
+}
